c818fecefe0579408e0d8a2aa5390426a1dac696
H3ARTgg <nomaxismax@gmail.com>
2023-07-08 18:42:21 +0300

    feat: Добавлен Error UI и кастомная кнопка

--- FakeNFT/Resources/Consts.swift
+++ FakeNFT/Resources/Consts.swift
@@ -6,7 +6,8 @@ struct Consts {
         static let profile = NSLocalizedString("profile.icon", comment: "Profile tabbar icon title")
         static let catalogue = NSLocalizedString("catalogue.icon", comment: "Catalogue tabbar icon title")
         static let cart = NSLocalizedString("cart.icon", comment: "Cart tabbar icon title")
-        static let statistics = NSLocalizedString("statistics.icon", comment: "Statistics tabbar icon title")    
+        static let statistics = NSLocalizedString("statistics.icon", comment: "Statistics tabbar icon title")
+        static let errorConnectionMessage = NSLocalizedString("error.connection.message", value: "Что-то пошло не так(", comment: "Connection error text")
     }
     
     struct Images {
