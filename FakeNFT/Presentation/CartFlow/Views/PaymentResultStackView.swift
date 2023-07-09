0ffd96ab56d397c0dd298f71ad7544d42b6c4373
JuliaRomanenko <rja-97@yandex.ru>
2023-07-10 00:52:47 +0300

    fix: Добавила элементы заглушки для stackView, чтобы констренты не ругались, когда данные с сервера еще не пришли

--- FakeNFT/Presentation/CartFlow/Views/PaymentResultStackView.swift
+++ FakeNFT/Presentation/CartFlow/Views/PaymentResultStackView.swift
@@ -1,6 +1,21 @@
 import UIKit
 
 final class PaymentResultStackView: UIStackView {
+    private let placeholderImageView: UIImageView = {
+         let view = UIImageView()
+         view.contentMode = .scaleAspectFit
+         return view
+     }()
+     
+     private let placeholderLabel: UILabel = {
+         let label = UILabel()
+         label.font = Consts.Fonts.bold22
+         label.textColor = Asset.Colors.ypBlack.color
+         label.textAlignment = .center
+         label.numberOfLines = 0
+         return label
+     }()
+    
     private let paymentResultImageView: UIImageView = {
         let view = UIImageView()
         view.contentMode = .scaleAspectFit
@@ -15,18 +30,38 @@ final class PaymentResultStackView: UIStackView {
         label.numberOfLines = 0
         return label
     }()
-
-    func configure(_ image: UIImage, textLabel: String) {
+    
+    init() {
+        super.init(frame: .zero)
+        
+        setupPlaceholders()
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupPlaceholders() {
         axis = .vertical
         distribution = .fill
         alignment = .fill
         spacing = 20
         translatesAutoresizingMaskIntoConstraints = false
-                
-        addElements()
         
+        [
+            placeholderImageView,
+            placeholderLabel
+        ].forEach { addArrangedSubview($0) }
+        
+        placeholderImageView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        placeholderLabel.heightAnchor.constraint(equalToConstant: 50).isActive = true
+    }
+
+    func configure(_ image: UIImage, _ textLabel: String) {
         paymentResultImageView.image = image
         paymentResultLabel.text = textLabel
+        
+        replaceElements()
     }
     
     private func addElements() {
@@ -34,5 +69,15 @@ final class PaymentResultStackView: UIStackView {
             paymentResultImageView,
             paymentResultLabel
         ].forEach { addArrangedSubview($0) }
+    }
+    
+    private func replaceElements() {
+        removeArrangedSubview(placeholderImageView)
+        removeArrangedSubview(placeholderLabel)
+        
+        placeholderImageView.removeFromSuperview()
+        placeholderLabel.removeFromSuperview()
+        
+        addElements()
     }
 }
